07b00f0be27cf4f3703a9b094d8bd238cc7f7fbe
ImVladislav <nafanya102@gmail.com>
2022-07-15 23:08:02 +0300

    work fucken script)

--- src/sass/layout/index.js
+++ src/sass/layout/index.js
@@ -1,3 +1,4 @@
+
 const prev = document.getElementById('btn-prev'),
     next = document.getElementById('btn-next'),
     slides = document.querySelectorAll('.slide'),
@@ -6,42 +7,33 @@ const prev = document.getElementById('btn-prev'),
 let index = 0;
 
 const activeSlide = n => {
+    console.log(n);
     for (slide of slides) {
         slide.classList.remove('active');
     }
      slides[n].classList.add('active');
 }
 
-const activeDot = n => {
-    for (dot of dots) {
-        dot.classList.remove('active_dot');
-    }
-     dots[n].classList.add('active_dot');
-}
-
-const prepareCurrentSlide = ind => {
-    activeSlide(ind);
-    activeDot(ind);
-}
-
 const nextSlide = () => {
     if (index == slides.length - 1) {
         index = 0;
-        prepareCurrentSlide(index);
+        activeSlide(index);
     } else {
         index++;
-        prepareCurrentSlide(index);
+        activeSlide(index);
         }  
 }
 
 const prevSlide = () => {
     if (index == 0){
         index = slides.length - 1
-        prepareCurrentSlide(index);
-    } else {
+        activeSlide(index);
+    }
+        else {
         index--;
-        prepareCurrentSlide(index);
+        activeSlide(index);
         }
+    
 }
 
 next.addEventListener('click', nextSlide);
