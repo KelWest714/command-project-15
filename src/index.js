e25bdff6cef5a78033d5f9341f156fbf2c00c12b
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-07-16 18:26:57 +0300

    add style

--- src/index.js
+++ src/index.js
@@ -27,3 +27,20 @@
     refs.body.classList.toggle('no-scroll');
   }
 })();
+// mobile js
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.mobile-menu-open'),
+    closeMenuBtn: document.querySelector('.mobile-menu-close'),
+    menu: document.querySelector('.menu'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-open');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
