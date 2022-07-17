cbc229db38b30c1a0f31b943b484401574d0219a
Andrii Ursolov <andrew.ursolov@gmail.com>
2022-07-17 19:11:09 +0300

    original/modals
    add contact modals
    add styles for 3 modals

--- src/index.js
+++ src/index.js
@@ -1,69 +1,69 @@
 //  ------------------------- Modals scripts ---------------------------
 
-document.addEventListener('DOMContentLoaded', () => {
-  let formButton = document.querySelector('[data-form-open]');
-  let geoButton = document.querySelector('[data-geo-open]');
+// document.addEventListener('DOMContentLoaded', () => {
+//   let formButton = document.querySelector('[data-form-open]');
+//   let geoButton = document.querySelector('[data-geo-open]');
 
-  let form = document.querySelector('[data-form]');
-  let geo = document.querySelector('[data-geo]');
+//   let form = document.querySelector('[data-form]');
+//   let geo = document.querySelector('[data-geo]');
 
-  let closeForm = document.getElementsByClassName('modal-close-form')[0];
-  let closeGeo = document.getElementsByClassName('modal-close-geo')[0];
+//   let closeForm = document.getElementsByClassName('modal-close-form')[0];
+//   let closeGeo = document.getElementsByClassName('modal-close-geo')[0];
 
-  let tagBody = document.querySelector('body');
+//   let tagBody = document.querySelector('body');
 
-  formButton.onclick = function (e) {
-    e.preventDefault();
-    form.classList.remove('is-hidden');
-    tagBody.classList.add('no-scroll');
-  };
+//   formButton.onclick = function (e) {
+//     e.preventDefault();
+//     form.classList.remove('is-hidden');
+//     tagBody.classList.add('no-scroll');
+//   };
 
-  geoButton.onclick = function (e) {
-    e.preventDefault();
-    geo.classList.remove('is-hidden');
-    tagBody.classList.add('no-scroll');
-  };
+//   geoButton.onclick = function (e) {
+//     e.preventDefault();
+//     geo.classList.remove('is-hidden');
+//     tagBody.classList.add('no-scroll');
+//   };
 
-  closeForm.onclick = function (e) {
-    e.preventDefault();
-    form.classList.add('is-hidden');
-    tagBody.classList.remove('no-scroll');
-  };
+//   closeForm.onclick = function (e) {
+//     e.preventDefault();
+//     form.classList.add('is-hidden');
+//     tagBody.classList.remove('no-scroll');
+//   };
 
-  closeGeo.onclick = function (e) {
-    e.preventDefault();
-    geo.classList.add('is-hidden');
-    tagBody.classList.remove('no-scroll');
-  };
+//   closeGeo.onclick = function (e) {
+//     e.preventDefault();
+//     geo.classList.add('is-hidden');
+//     tagBody.classList.remove('no-scroll');
+//   };
 
-  form.onmousedown = function (e) {
-    let target = e.target;
-    let modalContent = document.getElementsByClassName('backdrop-form');
-    if (target.closest('.' + modalContent.className) === null) {
-      form.classList.add('is-hidden');
-      tagBody.classList.remove('no-scroll');
-    }
-  };
+//   form.onmousedown = function (e) {
+//     let target = e.target;
+//     let modalContent = document.getElementsByClassName('backdrop-form');
+//     if (target.closest('.' + modalContent.className) === null) {
+//       form.classList.add('is-hidden');
+//       tagBody.classList.remove('no-scroll');
+//     }
+//   };
 
-  geo.onmousedown = function (e) {
-    let target = e.target;
-    let modalContent = document.getElementsByClassName('backdrop-geo');
-    if (target.closest('.' + modalContent.className) === null) {
-      geo.classList.add('is-hidden');
-      tagBody.classList.remove('no-scroll');
-    }
-  };
+//   geo.onmousedown = function (e) {
+//     let target = e.target;
+//     let modalContent = document.getElementsByClassName('backdrop-geo');
+//     if (target.closest('.' + modalContent.className) === null) {
+//       geo.classList.add('is-hidden');
+//       tagBody.classList.remove('no-scroll');
+//     }
+//   };
 
-  let openModals = document.getElementsByClassName('form');
+//   let openModals = document.getElementsByClassName('form');
 
-  for (let button of openModals) {
-    button.onclick = function (e) {
-      e.preventDefault();
-      form.classList.remove('is-hidden');
-      tagBody.classList.add('no-scroll');
-    };
-  }
-});
+//   for (let button of openModals) {
+//     button.onclick = function (e) {
+//       e.preventDefault();
+//       form.classList.remove('is-hidden');
+//       tagBody.classList.add('no-scroll');
+//     };
+//   }
+// });
 
 // ----------------------- Slider scripts-------------------------
 
@@ -166,8 +166,6 @@ dots.forEach((item, indexDot) => {
 
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
-
-})();
 
 // next.addEventListener('click', nextSlide);
 // prev.addEventListener('click', prevSlide);
