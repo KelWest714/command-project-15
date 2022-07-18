d0fc0ad04d8bf94a7cd9893c79a45177de77d276
Andrii Ursolov <andrew.ursolov@gmail.com>
2022-07-18 11:22:36 +0300

    original/modals
    update

--- src/partials/js/modals.js
+++ src/partials/js/modals.js
@@ -1,64 +1,57 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let formButton = document.querySelector('[data-form-open]');
-  let geoButton = document.querySelector('[data-geo-open]');
-
-  let form = document.querySelector('[data-form]');
-  let geo = document.querySelector('[data-geo]');
-
-  let closeForm = document.getElementsByClassName('modal-close-form')[0];
-  let closeGeo = document.getElementsByClassName('modal-close-geo')[0];
-
-  let tagBody = document.querySelector('body');
-
-  formButton.onclick = function (e) {
-    e.preventDefault();
-    form.classList.remove('is-hidden');
-    tagBody.classList.add('no-scroll');
+// modal header
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
-  geoButton.onclick = function (e) {
-    e.preventDefault();
-    geo.classList.remove('is-hidden');
-    tagBody.classList.add('no-scroll');
-  };
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  closeForm.onclick = function (e) {
-    e.preventDefault();
-    form.classList.add('is-hidden');
-    tagBody.classList.remove('no-scroll');
-  };
-
-  closeGeo.onclick = function (e) {
-    e.preventDefault();
-    geo.classList.add('is-hidden');
-    tagBody.classList.remove('no-scroll');
-  };
-
-  //   form.onmousedown = function (e) {
-  //     let target = e.target;
-  //     let modalContent = document.getElementsByClassName('backdrop');
-  //     if (target.closest('.' + modalContent.className) === null) {
-  //       form.classList.add('is-hidden');
-  //       tagBody.classList.remove('no-scroll');
-  //     }
-  //   };
-
-  //   geo.onmousedown = function (e) {
-  //     let target = e.target;
-  //     let modalContent = document.getElementsByClassName('backdrop-geo');
-  //     if (target.closest('.' + modalContent.className) === null) {
-  //       geo.classList.add('is-hidden');
-  //       tagBody.classList.remove('no-scroll');
-  //     }
-  //   };
-
-  let openModals = document.getElementsByClassName('form');
-
-  for (let button of openModals) {
-    button.onclick = function (e) {
-      e.preventDefault();
-      form.classList.remove('is-hidden');
-      tagBody.classList.add('no-scroll');
-    };
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
   }
-});
+})();
+// modal geo
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-geo-open]"),
+    closeModalBtn: document.querySelector("[data-geo-close]"),
+    modal: document.querySelector("[geo-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+// modal franchise
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[modal-franchise-open]"),
+    closeModalBtn: document.querySelector("[modal-franchise-close]"),
+    modal: document.querySelector("[franchise-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+// franchise JS
+// $(".name").focus(function(){
+//   $(".name-help").slideDown(500);
+// }).blur(function(){
+//   $(".name-help").slideUp(500);
+// });
+
+// $(".email").focus(function(){
+//   $(".email-help").slideDown(500);
+// }).blur(function(){
+//   $(".email-help").slideUp(500);
+// });
