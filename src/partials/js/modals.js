ed1bdaf5b8e54659fe18e1ced4d1c7dbf6d29c32
Andrii Ursolov <andrew.ursolov@gmail.com>
2022-07-18 19:51:25 +0300

    JS scripts update
    modals.js updated

--- src/partials/js/modals.js
+++ src/partials/js/modals.js
@@ -1,57 +1,41 @@
-// modal header
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    openGeoBtn: document.querySelector('[data-geo-open]'),
+    openFranchBtn: document.querySelector('[modal-franchise-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeGeoBtn: document.querySelector('[data-geo-close]'),
+    closeFrenchBtn: document.querySelector('[modal-franchise-close]'),
+    modal: document.querySelector('[data-modal]'),
+    geo: document.querySelector('[geo-modal]'),
+    franch: document.querySelector('[franchise-modal]'),
+    body: document.querySelector('body'),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openGeoBtn.addEventListener('click', toggleGeo);
+  refs.closeGeoBtn.addEventListener('click', toggleGeo);
+  refs.openFranchBtn.addEventListener('click', toggleFranch);
+  refs.closeFrenchBtn.addEventListener('click', toggleFranch);
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+  function toggleGeo() {
+    refs.geo.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+  function toggleFranch() {
+    refs.franch.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+  let openModal = document.querySelectorAll('[data-modal-open]');
+  for (let button of openModal) {
+    button.onclick = function (e) {
+      e.preventDefault();
+      refs.modal.classList.remove('is-hidden');
+      refs.body.classList.add('no-scroll');
+    };
   }
 })();
-// modal geo
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-geo-open]"),
-    closeModalBtn: document.querySelector("[data-geo-close]"),
-    modal: document.querySelector("[geo-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-// modal franchise
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[modal-franchise-open]"),
-    closeModalBtn: document.querySelector("[modal-franchise-close]"),
-    modal: document.querySelector("[franchise-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-// franchise JS
-// $(".name").focus(function(){
-//   $(".name-help").slideDown(500);
-// }).blur(function(){
-//   $(".name-help").slideUp(500);
-// });
-
-// $(".email").focus(function(){
-//   $(".email-help").slideDown(500);
-// }).blur(function(){
-//   $(".email-help").slideUp(500);
-// });
